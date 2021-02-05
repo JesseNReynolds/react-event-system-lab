0407c8b04bd5fbf7c5bc11e722f757a9ddfe0c79
JesseNReynolds <jessenreynolds@gmail.com>
2021-02-05 12:03:20 -0700

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
-// Code Keypad Component Here
+// Code Keypad Component Hereimport React from 'react';
+import React from 'react';
+
+export default class Keypad extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <input type='password' 
+                onKeyUp={this.sayTheLineBart}>
+
+                </input>
+            </div>
+        )
+    }
+
+    sayTheLineBart() {
+        console.log('Entering password...')
+    }
+
+}
+
